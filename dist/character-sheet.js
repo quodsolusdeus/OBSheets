f749c60e6ed0345ce3a23f737792a0a4e30e0f5c
quodsolusdeus <char.rossingtonsmith@gmail.com>
2024-12-08 21:28:35 +0000

    Build production version of character sheet

---
+++ dist/character-sheet.js
@@ -0,0 +1,13 @@
+[
+    {
+      "name": "Arden the Brave",
+      "stats": { "STR": 15, "DEX": 12, "HP": 45 },
+      "notes": "Loyal knight of the realm."
+    },
+    {
+      "name": "Lyra the Mystic",
+      "stats": { "INT": 18, "WIS": 14, "MP": 30 },
+      "notes": "Seeker of forbidden knowledge."
+    }
+  ]
+  
